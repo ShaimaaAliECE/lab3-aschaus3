c8e9ff1306c8f6dc9a603cf65a3f7b18075763b7
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-14 15:49:11 -0500

    small change

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,7 @@ let strokeWidth = 4  //Sets the width of the pencil
     //Sets up the drawing canvas
 function setup() {
 	const cv = createCanvas(800, 600)
-	cv.position(350, 100)
+    cv.position(350, 100)
 	cv.background(0)
 }
 
